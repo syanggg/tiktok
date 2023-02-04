2e01f57d0d9dcf67a9594a74c5d9ce8333fec4f3
syang <256093303@qq.com>
2023-02-04 19:23:00 +0800

    2023/2/4

--- middleware/jwt/jwt_test.go
+++ middleware/jwt/jwt_test.go
@@ -3,20 +3,21 @@ package middleware
 import (
 	"fmt"
 	"testing"
+	"tiktok/middleware/jwt"
 	"tiktok/models"
 )
 
 func TestJwtMiddleware(t *testing.T) {
 	login := &models.UserLogin{UserInfoId: 10}
 
-	token,err := ReleaseToken(login)
+	token,err := jwt.ReleaseToken(login)
 	if err != nil{
 		fmt.Println(err)
 	}
 
 	fmt.Println(token)
 
-	claims,err := ParseToken(token)
+	claims,err := jwt.ParseToken(token)
 	if err != nil{
 		fmt.Println(err)
 	}
